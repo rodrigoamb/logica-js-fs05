09e2f74e378f12c9900d0a43fe3b2707c87318cc
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2025-07-09 21:43:37 -0300

    exercicios

--- condicionais.js
+++ condicionais.js
@@ -31,19 +31,84 @@ console.log(resultado);
 
 // 1.	Crie uma função que receba um número e exiba se ele é positivo, negativo ou zero.
 
+function verificaNumero(numero) {
+  if (numero < 0) {
+    console.log("NEGATIVO");
+  } else if (numero > 0) {
+    console.log("POSITIVO");
+  } else {
+    console.log("O NUMERO é ZERO");
+  }
+}
+
+console.log(verificaNumero(3));
+console.log(verificaNumero(-5));
+console.log(verificaNumero(0));
+
 // 2.	Crie uma função que receba o nome de uma pessoa. Se o nome tiver mais de 5 letras, retorne “Nome longo”, senão “Nome curto”.
+
+function verificaNome(nome) {
+  if (nome.length > 5) {
+    return "Nome longo";
+  } else {
+    return "Nome curto";
+  }
+}
+
+console.log(verificaNome("Ana"));
+console.log(verificaNome("Rodrigo"));
 
 // 3.	Crie uma função que receba a idade de uma pessoa e retorne se ela é “Criança”, “Adolescente”, “Adulto” ou “Idoso”.
 
+function classificarIdade(idade) {
+  if (idade < 0) {
+    return "Digite um numero positivo";
+  }
+
+  if (idade < 12) {
+    return "Criança";
+  } else if (idade < 18) {
+    return "Adolecente";
+  } else if (idade < 60) {
+    return "Adulto ";
+  } else {
+    return "Idoso";
+  }
+}
+
 // 4.	Crie uma função que receba dois números e retorne o maior deles.
+
+function maiorNumero(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
 // 5.	Crie uma função que receba um número e retorne true se for par, e false se for ímpar.
 
+function numeroPar(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 // 6.	Crie uma função que receba o valor de uma compra. Se o valor for maior que 100, aplique 10% de desconto e retorne o novo valor.
+
+function aplicaDesconto(valor) {
+  if (valor > 100) {
+    return valor * 0.9;
+  } else {
+    return valor;
+  }
+}
 
 // 7.	Crie uma função que receba o nome de um aluno e sua nota. Retorne “Aprovado” se a nota for >= 6, senão “Reprovado”.
 
-// 8.	Crie uma função que receba dois números e retorne se eles são iguais, diferentes, ou se um é maior que o outro.
+// 8.	Crie uma função que receba dois números e retorne se eles são iguais, se forem diferentes, qual deles é o maior.
 
 // 9.	Crie uma função que receba um número e informe se ele é múltiplo de 3, de 5, de ambos ou de nenhum.
 
